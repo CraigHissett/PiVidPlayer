4c1c40aa11f2ae814fa245548828935d45262bd5
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-07-01 01:51:20 +0100

    Update playlists.py

--- playlists.py
+++ playlists.py
@@ -1,36 +1,43 @@
-import os, subprocess, sys
+import os, subprocess, sys, string
+from ctypes import windll
 
-FoundFolder = "J:\Films\Peppa Pig"
+PlaylistTitle = "Peppa Pig"
 
-#for root, dirs, files in os.walk(r'FoundFolder'):
-#    for file in files:
-#        if file.endswith('.mp4'):
+def DriveList():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+            SearchDrive(PlaylistTitle, letter +':')
+        bitmask >>= 1
+    #return drives    
 
-
-def ListDrives():
-    print(os.listdir(FoundFolder))
-    
-def CompilePlaylist(PlaylistTitle):
-    #Create a text file, step through folder and copy files into text file
+def SearchDrive(PlaylistTitle, DriveLetter):
+    #Create a text file, step through folder and copy file locations into text file
     PlaylistText = open(PlaylistTitle + '.txt', "w")
-    print(PlaylistTitle)
-    #for root, dirs, files in os.walk(".", topdown=True):
-    for root, dirs, files in os.walk(FoundFolder, topdown=True):
+    #print(PlaylistTitle)
+    print(DriveLetter)
+    for root, dirs, files in os.walk(DriveLetter, topdown=True):
         for name in files:
-            print(os.path.join(root, name))
-            PlaylistText.write(os.path.join(root, name) + '\n')
+            if PlaylistTitle in name:
+                print(os.path.join(root, name))
+                #if file.endswith('.mp4'):
+                PlaylistText.write(os.path.join(root, name) + '\n')
         for name in dirs:
+            #if PlaylistTitle in name:
             print(os.path.join(root, name))
-            PlaylistText.write(os.path.join(root, name) + '\n')
     PlaylistText.close()
 
 #def LoadPlaylist(PlaylistTitle):
     #For the Selected Playlist loop through the text file and create a list
     #LoadedPlaylist = open(PlaylistTitle + '.txt', 'r')
     #print LoadedPlaylist.read()
+    #PlayListText.read().split('\n')
     
 def Main():
-    ListDrives()
-    CompilePlaylist('Peppa')
+    DriveList()
+    
+#if __name__ == '__main__':
 
 Main()
