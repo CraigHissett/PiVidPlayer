3ef03ed3995fc3c48341aea66a172ffd5dec0a3f
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-07-29 15:10:17 +0100

    Create PiVidPlayer.py
    PyVidPlayer.py will be the main script, utilising all other scripts written.
    Running this should generate the welcome splash screen and then handle the 
    playing commands, as requested via the user's button presses.

---
+++ PiVidPlayer.py
@@ -0,0 +1,17 @@
+
+
+#This script needs to do the following:
+
+
+#def Startup()
+  #Run a splashscreen - use tkinter or pygame
+  #Run a refresh on playlists
+  #Load a screen to say "Push a button" or tirgger a sound to prompt
+
+#def ButtonCommands()
+  #Monitor button presses
+  #carry out actions
+  
+#def Menu()
+  #potentially create a class for menus
+  #and submenus
