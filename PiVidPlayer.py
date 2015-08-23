67be7f48ec0291b9004d65ed64bca4d76863653b
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-08-23 02:06:25 +0100

    Update PiVidPlayer.py
    Main script - handles GUI, and co-ordinates operation

--- PiVidPlayer.py
+++ PiVidPlayer.py
@@ -1,17 +1,79 @@
+import pygame, sys, time
+from pygame.locals import *
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('PiVidPlayer')
 
+#Colours setup
+#             R    G    B
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+BLUE      = (  0,   0, 255)
+DARKGREEN = (  0, 155,   0)
+DARKGRAY  = ( 40,  40,  40)
+BGCOLOR = BLACK
 
-#This script needs to do the following:
+def CheckForKeyPress():
+    #Escape button on keyboard closes, for debug purposes
+    #The For event below to be edited to capture GPIO buttons
+    #explore setting up buttons as a cloass to be imported
+    if len(pygame.event.get(QUIT)) > 0:
+        terminate()
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
 
+#    for event in pygame.event.get(): # event handling loop
+#        if event.type == QUIT:
+#            terminate()
+#        elif event.type == KEYDOWN:
+#            if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
+#                direction = LEFT
+#            elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
+#                direction = RIGHT
+#            elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
+#                direction = UP
+#            elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
+#                direction = DOWN
+#            elif event.key == K_ESCAPE:
+#                terminate()
 
-#def Startup()
-  #Run a splashscreen - use tkinter or pygame
-  #Run a refresh on playlists
-  #Load a screen to say "Push a button" or tirgger a sound to prompt
+def terminate():
+    pygame.quit()
+    sys.exit()
 
-#def ButtonCommands()
-  #Monitor button presses
-  #carry out actions
-  
-#def Menu()
-  #potentially create a class for menus
-  #and submenus
+def ShowStartScreen():
+    fontObj = pygame.font.Font('freesansbold.ttf', 32)
+    textSurfaceObj = fontObj.render('Hello, Isaac!', True, GREEN, BLACK)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (400, 300)
+    DISPLAYSURF.fill(BGCOLOR)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+    pygame.display.update()
+    time.sleep(5)
+
+def DrawPressKeyMsg():
+    fontObj = pygame.font.Font('freesansbold.ttf', 24)
+    textSurfaceObj = fontObj.render('Press a button to play.', True, DARKGRAY)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (400, 300)
+    DISPLAYSURF.fill(BGCOLOR)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+    pygame.display.update()
+    
+def Main():
+    ShowStartScreen()
+    DrawPressKeyMsg()
+#    if CheckForKeyPress():
+#        pygame.event.get() # clear event queue
+#        return
+    while True:
+        CheckForKeyPress()
+
+if __name__ == '__main__':
+    Main()
