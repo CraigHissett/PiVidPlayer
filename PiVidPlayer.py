87ccd23fae0be8595b7411497ab7975b99928356
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-08-26 00:17:32 +0100

    Update PiVidPlayer.py

--- PiVidPlayer.py
+++ PiVidPlayer.py
@@ -1,7 +1,7 @@
 import pygame, sys, time
 from pygame.locals import *
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((800, 600))
+DISPLAYSURF = pygame.display.set_mode((800, 480))
 pygame.display.set_caption('PiVidPlayer')
 
 #Colours setup
