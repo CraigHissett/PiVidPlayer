a761a616a98d7a862ff6b75c52fb89b0ae5b5d68
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-08-27 00:09:12 +0100

    Update main.py
    Tidied up my proposed code to run.
    decided to move OMXplayng to be written/handled in playlists.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import sys, time
 
 #import everything required from my python files:
-import GUI, import, playlists
+import GUI, buttons, playlists
 
 #import other requirements
 from pyomxplayer import OMXPlayer 
@@ -31,39 +31,33 @@ def Main():
       return
     elif buttons.pressed==LB1
       #Select Playlist1, Load video 1
-      playlist="Peppa.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))
+      playlists.play("Peppa.txt", 1)
     elif buttons.pressed==LB2
       #Select Playlist2
-      playlist="Barney.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))
+      playlists.play("Barney.txt", 1)
     elif buttons.pressed==LB3
       #Select Playlist3
-      playlist="Minions.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))     
+      playlists.play("Minions.txt", 1)
     elif buttons.pressed==RB1
       #Select Playlist1, Load video 1
-      playlist="Peppa.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))
+      playlists.play("Peppa.txt", 1)
     elif buttons.pressed==RB2
       #Select Playlist2
-      playlist="Barney.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))
+      playlists.play("Barney.txt", 1)
     elif buttons.pressed==RB3
       #Select Playlist3
-      playlist="Minions.txt"
-      omx=OMXPlayer(playlist.select(playlist, 1))
-    elif buttons.pressed==CB1
+      playlists.play("Minions.txt", 1)
+    elif buttons.pressed==CB1  
       #Select previous video
-      omx=OMXPlayer(playlist.select(playlist, -1))
-    elif buttons.pressed==CB1
+      playlists.skip(-1)
+    elif buttons.pressed==CB2
       #Play/Pause
-      omx.toggle_pause()
+      playlists.playpause()
       return
     elif buttons.pressed==CB3
       #Select next video
-      omx=OMXPlayer(playlist.select(playlist, +1))
-    
+      playlists.skip(+1)
+  
     
     
 if __name__ == '__main__': 
