b71f849beaf15647b26083412d9c253a946b0cb6
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-08-26 23:29:15 +0100

    Update buttons.py
    def pressed() added - will return the pin number for now.
    To do - get it to return a clearer value, such as the LB1-3, RB1-3, CB1-3 names.

--- buttons.py
+++ buttons.py
@@ -22,13 +22,19 @@ pins = [LB1, LB2, LB3, RB1, RB2, RB3, CB1, CB2, CB3]
 for pin in pins:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    for pin in pins:
-        input_state = GPIO.input(pin)
-        if input_state == False:
-            print(pin)
-            return pin
-            time.sleep(0.2)
+def pressed():
+    while True:
+        for pin in pins:
+            input_state = GPIO.input(pin)
+            if input_state == False:
+                print(pin)
+                return pin
+        time.sleep(0.2)
  
 # Add below line to reset all pin status on close       
 GPIO.cleanup()
+
+if __name__ == '__main__': 
+   pressed() 
+
+
