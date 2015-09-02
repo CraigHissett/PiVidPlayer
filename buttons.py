0c9ffc9b25a4088be4c62db2f2740e627c786819
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-09-02 09:31:53 +0100

    Update buttons.py
    Switched the setmode from BCM to board, so I can reference actual pin numbers.
    Easier for me to trace.

--- buttons.py
+++ buttons.py
@@ -2,19 +2,19 @@ import RPi.GPIO as GPIO
 import time
 
 # set buttons to use Broadcom naming convention
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Set up the button pins, 3 banks of 3
 
-LB1 = 14    # Actual pin 8 
-LB2 = 15    # Actual pin 10
-LB3 = 18    # Actual pin 12
-RB1 = 17    # Actual pin 11
-RB2 = 21    # Actual pin 13
-RB3 = 22    # Actual pin 15
-CB1 = 10    # Actual pin 19
-CB2 = 9    # Actual pin 21
-CB3 = 11    # Actual pin 23
+LB1 = 8
+LB2 = 10
+LB3 = 12
+RB1 = 11
+RB2 = 13
+RB3 = 15
+CB1 = 19
+CB2 = 21
+CB3 = 23
 
 pins = [LB1, LB2, LB3, RB1, RB2, RB3, CB1, CB2, CB3]
 # Using internal pull up resistor; no need for one in circuit; other pin on buttons to ground
