2942f9a5e136a91831d0274566b35ba3ad8429d4
Craig Hissett <CraigHissett@users.noreply.github.com>
2015-09-07 12:01:53 +0100

    Update GUI.py
    Removed the buttons checking to put in buttons.py

--- GUI.py
+++ GUI.py
@@ -45,78 +45,12 @@ def load(ScreenName, ScreenText):
         DISPLAYSURF.blit(textSurfaceObj2, textRectObj2)
         pygame.display.update()
 
-def CheckForKeyPress():
-    global player
-    #Escape button on keyboard closes, for debug purposes
-    #The For event below to be edited to capture GPIO buttons
-    #explore setting up buttons.py as defs to be called and a value returned etc?
-    if len(pygame.event.get(QUIT)) > 0:
-        terminate()
-    keyUpEvents = pygame.event.get(KEYUP)
-    if len(keyUpEvents) == 0:
-        return None
-    if keyUpEvents[0].key == K_ESCAPE:
-        terminate()
-    return keyUpEvents[0].key
-    if buttons.pressed==0
-      #Do nothing
-      return
-    elif buttons.pressed==LB1
-      #Select Playlist1, Load video 1
-      player = player.Player("Peppa.txt")
-    elif buttons.pressed==LB2
-      #Select Playlist2
-      player = player.Player("Barney.txt")
-    elif buttons.pressed==LB3
-      #Select Playlist3
-      player = player.Player("Minions.txt")
-    elif buttons.pressed==RB1
-      #Select Playlist1, Load video 1
-      player = player.Player("Peppa.txt")
-    elif buttons.pressed==RB2
-      #Select Playlist2
-      player = player.Player("Barney.txt")
-    elif buttons.pressed==RB3
-      #Select Playlist3
-      player = player.Player("Minions.txt")
-    elif buttons.pressed==CB1  
-      #Select previous video
-      player.Player.prev()
-    elif buttons.pressed==CB2
-      #Play/Pause
-      player.Player.Play()
-      return
-    elif buttons.pressed==CB3
-      #Select next video
-      player.Player.Next()
-#   Further events handling - to be integrated
-#Look for mouse functions too - use touch screen for input!
-#    for event in pygame.event.get(): 
-#        if event.type == QUIT:
-#            terminate()
-#        elif event.type == KEYDOWN:
-#            if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
-#                direction = LEFT
-#            elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
-#                direction = RIGHT
-#            elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
-#                direction = UP
-#            elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
-#                direction = DOWN
-#            elif event.key == K_ESCAPE:
-#                terminate()
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
 def Main():
     load("StartScreen","")
     time.sleep(5)
     load("PressAnyKey","")
     while True:
-        CheckForKeyPress()
+        buttons.CheckForKeyPress()
 
 if __name__ == '__main__':
     Main()
